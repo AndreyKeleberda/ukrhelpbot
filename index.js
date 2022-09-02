17a632a89e762fe4d970bc3d3fba879882d58bbb
AndreyKeleberda <74091304+AndreyKeleberda@users.noreply.github.com>
2022-09-02 20:47:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const token = '5223300284:AAGr1kPhF848KMmzjoAfgtLnDFSeG1yNlPA'
 const bot = new telegramApi(token, { polling: true })
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+bot.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 // база данных
