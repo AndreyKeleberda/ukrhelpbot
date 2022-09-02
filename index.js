e6e00adff08cf8fcd2ccd3903f91769144eb5d4b
AndreyKeleberda <74091304+AndreyKeleberda@users.noreply.github.com>
2022-09-02 21:08:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const telegramApi = require('node-telegram-bot-api')
 const token = '5223300284:AAGr1kPhF848KMmzjoAfgtLnDFSeG1yNlPA'
 const bot = new telegramApi(token, { polling: true })
-setInterval(() => {
-  http.get("http://yourapp.herokuapp.com/", (res) => {})
-}, 1 * 60 * 1000)
+const PORT = process.env.PORT || 3000;
 // база данных
 let bd = {
   kyiv: {
